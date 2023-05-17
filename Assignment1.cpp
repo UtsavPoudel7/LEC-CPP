34b4c8b04f953d114f9ea1870f4ad65ecaa6ac5c
Utsav Poudel <utsavpoudel057@gamil.com>
2023-05-17 22:30:13 +0545

    Assignment_1

---
+++ Assignment1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Room
+{
+public:
+    int length;
+    int breadth;
+    int height;
+
+    double calculateArea()
+    {
+        return length * breadth;
+    }
+    double calculateVolume()
+    {
+        return length * breadth * height;
+    }
+
+    void Input()
+    {
+        cout<<"Enter the length of the room : ";
+        cin>>length;
+
+        cout<<"Enter the breadth of the room : ";
+        cin>>breadth;
+
+        cout<<"Enter the height of the room : ";
+        cin>>height;
+    }
+};
+
+int main()
+{
+    Room obj401;
+    obj401.Input();
+
+    cout <<"The area of the room is :" << obj401.calculateArea()<<endl;
+    cout <<"The volume of the room is :" << obj401.calculateVolume()<<endl;
+
+    return 0;
+}
---
+++ area_vol.cpp
@@ -0,0 +1,67 @@
+// #include <iostream>
+// using namespace std;
+
+// class Room
+// {
+// public:
+//     int length;
+//     int breadth;
+//     int height;
+
+//     double calculateArea()
+//     {
+//         return length*breadth;
+//     }
+//     double calculateVolume()
+//     {
+//         return length*breadth*height;
+//     }
+// };
+
+// int main()
+// {
+//     Room obj401;
+//     obj401.length=10;
+//     obj401.breadth=20;
+//     obj401.height=30;
+
+//     cout<<"The area of the room is :"<<obj401.calculateArea();
+//     cout<<"The volume of the room is:"<<obj401.calculateVolume();
+// }
+
+
+#include <iostream>
+using namespace std;
+
+class Room
+{
+public:
+    int length;
+    int breadth;
+    int height;
+
+    void calculateArea(int a, int b)
+    {
+        length=a;
+        breadth=b;
+       cout<<"The area of the room is:"<<length*breadth;
+    }
+    void calculateVolume(int c,int d,int e)
+    {
+        length=c;
+        breadth=d;
+        height=e;
+        cout<<"The area of the room is :"<<length*breadth*height;
+    }
+};
+
+int main()
+{
+    Room obj401;
+    obj401.calculateArea(10,20);
+
+    obj401.calculateVolume(10,20,40);
+}
+
+
+//Assignment :: Write the same code but ask the value by the user.
